6aec7083923dce5b34c2ee3f762ff809f9152757
zzzzer <zzzzer91@gmail.com>
2023-05-05 16:04:11 +0800

    feat: add CopyContext function

---
+++ context.go
@@ -0,0 +1,15 @@
+package zlog
+
+import "context"
+
+// CopyContext 只保留原来 context 中指定字段
+func CopyContext(ctx context.Context) context.Context {
+	newCtx := context.Background()
+	if v := ctx.Value(EntityFieldNameTraceId); v != nil {
+		newCtx = context.WithValue(newCtx, EntityFieldNameTraceId, v)
+	}
+	if v := ctx.Value(EntityFieldNameRequestId); v != nil {
+		newCtx = context.WithValue(newCtx, EntityFieldNameRequestId, v)
+	}
+	return newCtx
+}
